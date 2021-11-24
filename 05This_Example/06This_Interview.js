c470a08dc2e61f1b983760faa2a6ea3aa9a799da
EdwardZhangJ <edward_job@163.com>
2021-11-24 14:52:14 +0800

    day 05 this 绑定实例 this绑定面试题

--- 05This_Example/06This_Interview.js
+++ 05This_Example/06This_Interview.js
@@ -23,25 +23,25 @@
 // sayName();
 
 // 面试题二
-var name = "window";
-var person1 = {
-    name: "person1",
-    foo1: function () {
-        console.log(this.name)
-    },
-    foo2: () => console.log(this.name),
-    foo3: function () {
-        return function () {
-            console.log(this.name)
-        }
-    },
-    foo4: function () {
-        return () => {
-            console.log(this.name)
-        }
-    }
-};
-var person2 = { name: 'person2'};
+// var name = "window";
+// var person1 = {
+//     name: "person1",
+//     foo1: function () {
+//         console.log(this.name)
+//     },
+//     foo2: () => console.log(this.name),
+//     foo3: function () {
+//         return function () {
+//             console.log(this.name)
+//         }
+//     },
+//     foo4: function () {
+//         return () => {
+//             console.log(this.name)
+//         }
+//     }
+// };
+// var person2 = { name: 'person2'};
 
 // person1.foo1(); // person1：隐式绑定
 // person1.foo1.call(person2); // person2：显示绑定优先级大于隐式绑定
@@ -53,6 +53,30 @@ var person2 = { name: 'person2'};
 // person1.foo3.call(person2)(); // window 独立函数调用
 // person1.foo3().call(person2); // person2 显示绑定
 
-person1.foo4()(); // person1 箭头函数不绑定 this 上层作用域是 person1
-person1.foo4.call(person2)() // person2 上层作用被显示的绑定到 person2
-person1.foo4().call(person2) // person1 上层作用域是 person1
+// person1.foo4()(); // person1 箭头函数不绑定 this 上层作用域是 person1
+// person1.foo4.call(person2)() // person2 上层作用被显示的绑定到 person2
+// person1.foo4().call(person2) // person1 上层作用域是 person1
+
+// 面试题三
+
+var name = "window";
+function Person (name) {
+    this.name = name;
+    this.foo1 = function () {
+        console.log(this.name)
+    };
+    this.foo2 = () => console.log(this.name);
+    this.foo3 = function () {
+        return function () {
+            console.log(this.name)
+        }
+    };
+    this.foo4 = function () {
+        return () => {
+            console.log(this.name)
+        }
+    }
+}
+
+var person1 = new Person('person1');
+var person2 = new Person('person2')
