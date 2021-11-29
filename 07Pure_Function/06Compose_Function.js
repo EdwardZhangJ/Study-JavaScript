4560963bb94724b487815b3a11600c1c5d1f46cc
EdwardZhangJ <edward_job@163.com>
2021-11-29 16:50:34 +0800

    day 07 纯函数、函数的柯里化、组合函数

---
+++ 07Pure_Function/06Compose_Function.js
@@ -0,0 +1,14 @@
+/**
+ * @06Compose_Function.js
+ * @description 组合函数的理解
+ * @author zhangjun
+ * @update (zhangjun 2021/11/29)
+ **/
+
+function double(num) {
+    return num * 2;
+}
+
+function square(num) {
+    return num * num
+}
