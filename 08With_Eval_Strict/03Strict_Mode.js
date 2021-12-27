d9da99074082e0e6e79f3c2d8cf24ca13793e1bc
EdwardZhangJ <edward_job@163.com>
2021-12-27 19:04:53 +0800

    day 08 with eval Strict模式 介绍

--- 08With_Eval_Strict/03Strict_Mode.js
+++ 08With_Eval_Strict/03Strict_Mode.js
@@ -4,3 +4,16 @@
  * @author zhangjun
  * @update (zhangjun 2021/12/6)
  **/
+"use strict";
+
+var message = "Hello World";
+console.log(message);
+
+
+function foo() {
+    "use strict";
+    
+    true.foo = "abc";
+}
+
+foo();
