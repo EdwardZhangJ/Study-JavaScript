3af6752ac8534a0604eb0185ff48dc2a3d9a69fc
EdwardZhangJ <edward_job@163.com>
2022-04-21 13:41:33 +0800

    day12 ES6 实现 class extends 并使用 babel 转换为 ES5 源码

--- demo/01test.js
+++ demo/01test.js
@@ -5,6 +5,12 @@
  * @update (zhangjun 2022/1/14)
  **/
 
-let arr = [];
+// let arr = [];
 
-console.log(Object.prototype.toString.call(arr));
+// console.log(Object.prototype.toString.call(arr));
+
+
+let x = 1;
+let y = 2;
+[x, y] = [y, x];
+console.log(x, y);
